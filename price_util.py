0438cbb823a138a8f6193e787fd3e0f2b7216804
rosss <rosss@sfu.ca>
2021-02-20 20:40:16 -0800

    added saveImg()

--- price_util.py
+++ price_util.py
@@ -44,9 +44,40 @@ def getUrl( id ):
 		data = res.json()[0]
 		return data['website_url']
 
-print( currentPrice( 'BTC' ) )
-
 
 
 # input: ticker symbol in string
-# output: 
+# output:
+def getSparkline( id, s, e ):
+	reqUrl = "https://api.nomics.com/v1/currencies/sparkline?"
+	key = "key=" + mykey
+	ids = "&ids=" + id
+	start = "&start=" + s + "T00%3A00%3A00Z"
+	end = "&end=" + e + "T00%3A00%3A00Z"
+
+	res = requests.get( reqUrl + key + ids + start + end )
+
+	if( res.status_code != 200 ):
+		return None
+	else:
+		data = res.json()[0]
+		return [ data['timestamps'], data['prices'] ]
+
+
+
+# input: image url, image file name
+# output: N/A (saves an image file at current directory)
+def saveImg( url, filename ):
+	res = requests.get( url )
+	
+	f = open( filename, "wb" )
+	f.write( res.content )
+	f.close()
+
+
+#print( json.dumps( getSparkline( 'BTC', "2021-02-10", "2021-02-20" ), indent=2 ) )
+#saveImg( "https://s3.us-east-2.amazonaws.com/nomics-api/static/images/currencies/btc", "1.svg"  )
+
+
+
+
