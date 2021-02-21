513b5a88203f003ed660505a4555e032e908641e
rosss <rosss@sfu.ca>
2021-02-20 17:23:20 -0800

    edited currentPrice() to work properly

--- price_util.py
+++ price_util.py
@@ -7,23 +7,25 @@ import json
 mykey = os.getenv( 'NOMICS_API_KEY' )
 
 # input: ticker symbol in string
-# output: current price of the crypto
+# output: a list consisting timestamp and price of the crypto
 def currentPrice( id ):
 	ticker = "https://api.nomics.com/v1/currencies/ticker?"
 	key = "key=" + mykey
 	ids = "&ids=" + id
-	interval = "&interval=1d"
-	perPage = "&per-page=100"
-	page = "&page=1"
+#	interval = "&interval=1d"
+#	perPage = "&per-page=100"
+#	page = "&page=1"
 	
-	response = requests.get( ticker + key + ids + interval + perPage + page )
+	response = requests.get( ticker + key + ids )
 
 	if( response.status_code != 200 ):
 		return null
 	else:
-		formattedRes = json.dumps( response.json(), indent=2 )
+#		formattedRes = json.dumps( response.json(), indent=2 )
 
-		return formattedRes
+		data = response.json()[0]
+
+		return [ data['price'], data['price_timestamp'] ]
 
 
-
+print( currentPrice( 'BTC' ) )
